6825b7f1e64311071e643883e9cd19b88bf98d27
Ezequiel Rangel <chekelinho@gmail.com>
2022-12-22 18:11:40 -0700

    Homepage copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,57 @@
 <div class="wrapper">
-  <header class="header">
-    <a href="/">Ezequiel Rangel</a>
-  </header>
-  <section id="intro">
-    <div class="hero">
-      <h1 class="hero__title">
-        <span>
-          I design Digital
-          <br />
-          products, web design,
-          <br /> and branding.
-        </span>
-      </h1>
-      <p class="hero__copy">
-        Bring a business mind to design, and transform your career and company, with this free book by Apple and Electronic Arts design veteran Ryan Rumsey. Inside, he shares the essential vocabulary frameworks that you can put right to work.
-      </p>
-      <a href="/about" class="hero__cta">
-        About me
-        <span>&rarr;</span>
-      </a>
-    </div>
-  </section>
+	<header class="header">
+		<a href="/">Ezequiel Rangel</a>
+	</header>
+	<section id="intro">
+		<div class="hero">
+			<h1 class="hero__title">
+				<span>
+					I build websites
+					<br />
+					that makes Google
+					<br />
+					<a href="https://pagespeed.web.dev/" rel="noopener noreferrer" target="_blank">
+						PageSpeed
+					</a>
+					happy
+				</span>
+			</h1>
+			<p class="hero__copy">
+				I am a Frontend Developer deeply interested in Javascript applications. I use the latest
+				technology available to deliver highly performant products that saves money and time.
+			</p>
+			<a href="#" class="hero__cta">
+				Let me prove it
+				<span>&rarr;</span>
+			</a>
+		</div>
+	</section>
 </div>
 
 <style>
-  .hero__title{
-    @apply text-4xl leading-[4rem] font-display mb-12 font-normal;
-    @apply md:leading-[6rem] md:text-[4.3rem];
-  }
+	.hero__title {
+		@apply text-4xl leading-[4rem] font-display mb-12 font-normal;
+		@apply md:leading-[6rem] md:text-[4.3rem];
+	}
 
-  .hero__title > span{
-    @apply border-b-[15px] border-featured;
-  }
+	.hero__title > span {
+		@apply border-b-[15px] border-featured;
+	}
 
-  .hero__copy{
-    @apply leading-7 mb-12 max-w-xl;
-  }
+	.hero__copy {
+		@apply leading-7 mb-12 max-w-xl;
+	}
 
-  .hero__cta{
-    @apply font-semibold transition-colors;
-    @apply hover:text-featured-dark;
-  }
+	.hero__cta {
+		@apply font-semibold transition-colors;
+		@apply hover:text-featured-dark;
+	}
 
-  .hero__cta > span{
-    @apply transition-transform translate-x-0 inline-block;
-  }
+	.hero__cta > span {
+		@apply transition-transform translate-x-0 inline-block;
+	}
 
-  .hero__cta:hover > span{
-    @apply translate-x-4;
-  }
+	.hero__cta:hover > span {
+		@apply translate-x-4;
+	}
 </style>
