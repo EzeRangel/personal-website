896246dd695a0ddf3e6b7cfeb8753535bcd679b1
Ezequiel Rangel <chekelinho@gmail.com>
2023-07-24 13:21:56 -0700

    Adds links to external posts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,6 +24,61 @@
 			<span>&rarr;</span>
 		</a>
 	</section>
+	<section id="blog-list" class="page-section">
+		<div class="section-head">
+			<h2 class="heading heading--sub">Latest posts</h2>
+		</div>
+		<ul class="space-y-4">
+			<li>
+				<a
+					href="https://typeshare.co/ezerangel/posts/-MnccuWBHwavQTd9gvWg"
+					class="external-link"
+					target="_blank"
+				>
+					A tu cliente no le importa si usas TailwindCSS
+					<span>
+						<svg
+							width="15"
+							height="15"
+							viewBox="0 0 15 15"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							><path
+								d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
+								fill="currentColor"
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+							/></svg
+						>
+					</span>
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://typeshare.co/ezerangel/posts/-MjjrL-96b7vsNXXdth_"
+					class="external-link"
+					target="_blank"
+				>
+					Publicando en tiendas una app en Expo
+					<span>
+						<svg
+							width="15"
+							height="15"
+							viewBox="0 0 15 15"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+							><path
+								d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
+								fill="currentColor"
+								fill-rule="evenodd"
+								clip-rule="evenodd"
+							/></svg
+						>
+					</span>
+				</a>
+			</li>
+		</ul>
+	</section>
 	<section id="social-proof" class="page-section">
 		<div class="section-head">
 			<h2 class="heading heading--sub">Work</h2>
