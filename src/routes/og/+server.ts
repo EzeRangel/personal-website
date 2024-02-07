c43884730237acef5cf0510cc0da9e08567d49b8
Ezequiel Rangel <chekelinho@gmail.com>
2024-02-07 14:59:32 -0700

    Adds description to OG image generator function

--- src/routes/og/+server.ts
+++ src/routes/og/+server.ts
@@ -1,17 +1,25 @@
 import { ImageResponse } from "@ethercorps/sveltekit-og";
 import type { RequestHandler } from "@sveltejs/kit";
 
-const buildTemplate = (title: string, date?: string) => {
-	const dateTag = `<p class="font-body mb-6 text-xl font-bold capitalize">${date}</p>`;
+const buildTemplate = (title: string, date?: string, description?: string) => {
+	const dateTag = `<p class="font-body mb-2 text-xl font-bold capitalize">${date}</p>`;
+	const descriptionTag = `<div>
+					<p class="font-body text-4xl font-semibold">
+						${description}
+					</p>
+				</div>`;
 
 	return `
   <section tw="w-full h-full flex" style="background-image: linear-gradient(to right bottom, rgb(255, 228, 230), rgb(204, 251, 241))">
     <div tw="w-full h-full flex flex-col justify-between py-10 px-16">
-      <div tw="flex flex-col gap-6">
-        ${date !== undefined ? dateTag : ""}
-        <h1 tw="text-[80px] leading-[1] tracking-tighter">
-          ${title}
-        </h1>
+      <div tw="flex flex-col gap-10">
+        <div tw="flex">
+					${date !== undefined ? dateTag : ""}
+					<h1 tw="text-[80px] leading-[1] tracking-tighter">
+						${title}
+					</h1>
+				</div>
+				${description !== undefined ? descriptionTag : ""}
       </div>
       <div tw="flex">
         <span tw="rounded-full overflow-hidden inline-block">
@@ -30,9 +38,12 @@ const fontData = await fontFile.arrayBuffer();
 
 export const GET: RequestHandler = async ({ url }) => {
 	let date = undefined;
-	let title = "Building websites that makes Google PageSpeed happy • Ezequiel Rangel";
+	let title = "Building websites that save money and time • Ezequiel Rangel";
+	let description = undefined;
+
 	const hasTitle = url.searchParams.has("title");
 	const hasDate = url.searchParams.has("date");
+	const hasDescription = url.searchParams.has("description");
 
 	if (hasTitle) {
 		title = url.searchParams.get("title")?.slice(0, 100) as string;
@@ -44,7 +55,11 @@ export const GET: RequestHandler = async ({ url }) => {
 		date = parsed.toLocaleString("en-US", { dateStyle: "medium" });
 	}
 
-	const htmlTemplate = buildTemplate(title, date);
+	if (hasDescription) {
+		description = url.searchParams.get("description") as string;
+	}
+
+	const htmlTemplate = buildTemplate(title, date, description);
 
 	return await ImageResponse(htmlTemplate, {
 		height: 630,
