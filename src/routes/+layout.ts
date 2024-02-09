3bde4382e2c9e6f590f3361a6bda7c344fc7552c
Ezequiel Rangel <chekelinho@gmail.com>
2024-02-09 13:17:02 -0700

    Adds Posthog Analytics Integration

---
+++ src/routes/+layout.ts
@@ -0,0 +1,21 @@
+import posthog from "posthog-js";
+import { browser } from "$app/environment";
+import type { LayoutLoad } from "./$types";
+import { PUBLIC_POSTHOG_API_KEY } from "$env/static/public";
+
+export const load: LayoutLoad = async ({ data }) => {
+	if (browser) {
+		posthog.init(PUBLIC_POSTHOG_API_KEY, {
+			api_host: "https://app.posthog.com",
+			capture_pageview: false,
+			capture_pageleave: false,
+			loaded: (ph) => {
+				if (location.pathname === "localhost" || location.hostname === "127.0.0.1") {
+					ph.opt_out_capturing();
+				}
+			}
+		});
+	}
+
+	return data ?? {};
+};
