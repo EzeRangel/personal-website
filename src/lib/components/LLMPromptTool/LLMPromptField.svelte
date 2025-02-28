bdcf7a6aec91d5867545d519e3584d628fe6b261
Ezequiel Rangel <chekelinho@gmail.com>
2025-02-28 14:54:15 -0700

    Improves the LLMPromptField border animation

--- src/lib/components/LLMPromptTool/LLMPromptField.svelte
+++ src/lib/components/LLMPromptTool/LLMPromptField.svelte
@@ -5,18 +5,22 @@
 	export let text: string;
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<div
-	role="button"
-	tabindex="0"
-	class="w-full rounded-lg relative cursor-pointer p-2 bg-white border-2 border-transparent transition-all duration-300 aiBorderField"
-	on:mouseenter={() => (isVisible = true)}
-	on:mouseleave={() => (isVisible = false)}
-	on:click={() => (isVisible = !isVisible)}
->
-	<slot />
-	<div class="aiBorder" />
-	{#if isVisible}
-		<LlmPromptTool {text} />
-	{/if}
+<div class="overflow-hidden z-20 relative rounded-lg">
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div
+		role="button"
+		tabindex="0"
+		class="w-full rounded-lg relative cursor-pointer p-2 border-2 border-transparent transition-all duration-300 animated-border"
+		on:mouseenter={() => (isVisible = true)}
+		on:mouseleave={() => (isVisible = false)}
+		on:click={() => (isVisible = !isVisible)}
+	>
+		<slot />
+		<span class="bg-animated-border">
+			<span class="trail-animated-border" />
+		</span>
+		{#if isVisible}
+			<LlmPromptTool {text} />
+		{/if}
+	</div>
 </div>
