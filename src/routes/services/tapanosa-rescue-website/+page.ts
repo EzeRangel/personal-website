02e3fe6c1d1f048667e6c6020c240611ccff1588
Ezequiel Rangel <chekelinho@gmail.com>
2025-06-20 10:52:06 -0700

    Service landing page for Grupo Tapanosa

---
+++ src/routes/services/tapanosa-rescue-website/+page.ts
@@ -0,0 +1,12 @@
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = async () => {
+	return {
+		seo: {
+			title: "Quick Wins for Grupo Tapanosa Website | Ezequiel Rangel",
+			description:
+				"A quick expert review of an outdated website with clear suggestions to improve design, mobile performance, and user engagement.",
+			url: `https://ezerangel.com/services/tapanosa-rescue-website`
+		}
+	};
+};
