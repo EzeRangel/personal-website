7f55530547bbe02569986e65460180b2ca913987
Ezequiel Rangel <chekelinho@gmail.com>
2025-09-19 12:17:34 -0700

    Implements sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,48 @@
+const site = "https://ezerangel.com";
+const pages: string[] = [
+	"writing",
+	"reading",
+	"building",
+	"bookmarks",
+	"rescue-packages",
+	"es/rescue-packages",
+	"writing/icusa-case-study",
+	"writing/having-fun-with-async-iterators-and-apis",
+	"writing/previous-sibling-selector-css",
+	"writing/publishing-expo-apps",
+	"writing/upgrading-react-native-apps",
+	"writing/your-client-doesnt-mind-you-use-tailwindcss"
+];
+
+export async function GET({ url }) {
+	const body = sitemap(pages);
+	const response = new Response(body);
+
+	response.headers.set("Cache-Control", "max-age=0, s-max-age=3600");
+	response.headers.set("Content-Type", "application/xml");
+
+	return response;
+}
+
+const sitemap = (pages: string[]) => `<?xml version="1.0" encoding="UTF-8" ?>
+<urlset
+  xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+  xmlns:xhtml="https://www.w3.org/1999/xhtml"
+  xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+  xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+>
+  ${pages
+		.map(
+			(page) => `
+  <url>
+    <loc>${site}/${page}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.5</priority>
+  </url>
+  `
+		)
+		.join("")}
+</urlset>
+`;
