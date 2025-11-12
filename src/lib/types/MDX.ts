043ef383e15181caab145c8d0faad6ac9fc3bfb5
Ezequiel Rangel <chekelinho@gmail.com>
2025-11-12 13:28:41 -0700

    Adds a new blogpost

--- src/lib/types/MDX.ts
+++ src/lib/types/MDX.ts
@@ -5,9 +5,10 @@ export interface MDXPost {
 		published_at: string;
 		external?: boolean;
 		link?: string;
+		lang?: string;
 		updated_at?: string;
 		supports_prompt_mode?: boolean;
 	};
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+
 	default: any;
 }
